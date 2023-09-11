1bc44d42a127c0b8eeef87074987f01a6255a818
linnaanna <linna.anna@gmail.com>
2023-09-11 17:23:42 +0300

    initial commit

---
+++ task2.sql
@@ -0,0 +1,20 @@
+CREATE TABLE table10 (
+	id varchar PRIMARY KEY,
+	tammi1 varchar,
+	tammi2 varchar,
+	tammi3 varchar,
+	tammi4 varchar,
+	tammi5 varchar,
+	tammi6 varchar,
+	tammi7 varchar,
+	tammi8 varchar,
+	tammi9 varchar,
+	tammi10 varchar,
+	tammi11 varchar,
+	tammi12 varchar,
+	tammi13 varchar,
+	tammi14 varchar
+	
+);
+
+
