1ce7303f58b107c8b8494d0f341cc6f1d81b78b4
linnaanna <linna.anna@gmail.com>
2023-09-12 08:19:13 +0300

    function and exception handling

--- task1.py
+++ task1.py
@@ -1,31 +1,44 @@
 import json
 import psycopg2
 
+def create_and_insert():
 
+   con=None
+   try:
 # read JSON data from file
-with open('properties.json') as f:
-   moredata = json.load(f)
+      with open('properties.json') as f:
+         moredata = json.load(f)
+         
     
 # connect to the database
-conn = psycopg2.connect(host="localhost", database="testi2", user="postgres", password="Arwe1981")
+      conn = psycopg2.connect(host="localhost", database="testi2", user="postgres", password="Arwe1981")
 
 # create a cursor and table
 
-cur = conn.cursor()
-cur.execute("CREATE TABLE data_table3 ( id SERIAL PRIMARY KEY, location varchar, name varchar, code varchar);")
+      cur = conn.cursor()
+      cur.execute("CREATE TABLE propertydata ( id SERIAL PRIMARY KEY, location varchar, name varchar, code varchar);")
 
 # execute the INSERT statement
 
-sql = "INSERT INTO data_table3 (location, name, code) VALUES "
-vals = []
-for data in moredata:
-    vals.append("('%s', '%s', '%s')" %
+      sql = "INSERT INTO propertydata (location, name, code) VALUES "
+      vals = []
+      for data in moredata:
+         vals.append("('%s', '%s', '%s')" %
                 (data['locationName'], data['propertyName'], data['propertyCode']))
-cur.execute(sql + ",".join(vals))
+      cur.execute(sql + ",".join(vals))
 
 # commit the changes to the database
-conn.commit()
+      conn.commit()
 
 # close the cursor and connection
-cur.close()
-conn.close()
+      cur.close()
+      conn.close()
+
+   except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+   finally:
+         if con is not None:
+            con.close()
+
+
+create_and_insert()
