faa4494d3b64a7bf52c8be068a4b0bdf0cd64410
linnaanna <linna.anna@gmail.com>
2023-09-12 08:51:09 +0300

    wrote explanation

--- task3.sql
+++ task3.sql
@@ -1,20 +1,21 @@
-SELECT propertydata.id,
-propertydata.location,
-propertydata.code,
-eldatata.tammi1,
-eldatata.tammi2,
-eldatata.tammi3,
-eldatata.tammi4,
-eldatata.tammi5,
-eldatata.tammi6,
-eldatata.tammi7,
-eldatata.tammi8,
-eldatata.tammi9,
-eldatata.tammi10,
-eldatata.tammi11,
-eldatata.tammi12,
-eldatata.tammi13,
-eldatata.tammi14
-FROM propertydata
-INNER JOIN eldatata
-ON propertydata.code = eldatata.id
+SELECT propertydata.id, propertydata.location, propertydata.code,
+    eldata.tammi1,
+    eldata.tammi2,
+    eldata.tammi3,
+    eldata.tammi4,
+    eldata.tammi5,
+    eldata.tammi6,
+    eldata.tammi7,
+    eldata.tammi8,
+    eldata.tammi9,
+    eldata.tammi10,
+    eldata.tammi11,
+    eldata.tammi12,
+    eldata.tammi13,
+    eldata.tammi14
+FROM propertydata INNER JOIN eldata
+ON propertydata.code = eldata.id
+
+Yhdistin taulut inner joinilla käyttäen property codea ja id:tä, jolloin yhdistettyyn 
+tauluun tulee selvyyden vuoksi tietoja taulusta propertydata vain sellaisista kiinteistöistä, 
+joiden sähkönkäyttö löytyy taulusta eldata.
